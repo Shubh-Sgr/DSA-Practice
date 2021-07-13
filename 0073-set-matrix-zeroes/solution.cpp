d1d3045f5ada860272125448996c7295cfc1973f
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2021-07-13 04:16:46 +0000

    Sync LeetCode submission Runtime - 24 ms (9.64%), Memory - 13.2 MB (100.00%)

--- 0073-set-matrix-zeroes/solution.cpp
+++ 0073-set-matrix-zeroes/solution.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int r=0,c=0;
+        int r=0;
+        int c=0;
         int n=matrix.size();
         int m=matrix[0].size();
         for (int i=0; i<n; i++)
@@ -10,24 +11,41 @@ public:
             {
                 if (matrix[i][j]==0)
                 {
-                    if (i==0) r=1;
-                     if (j==0) c=1;
+                    if (i==0)
+                    {
+                        r=1;
+                    }
+                    if (j==0)
+                    {
+                        c=1;
+                    }
                     matrix[0][j]=0;
                     matrix[i][0]=0;
                 }
             }
         }
-        for(int i = 1; i < matrix.size(); i++){
-            for(int j = 1; j < matrix[0].size(); j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+        for (int i=1; i<n; i++)
+        {
+            for (int j=1; j<m; j++)
+            {
+                if (matrix[0][j]==0 || matrix[i][0]==0)
+                {
+                    matrix[i][j]=0;                }
             }
         }
-        
-        if(c){
-            for(int i = 0; i < matrix.size(); i++) matrix[i][0] = 0;
+        if (r)
+        {
+            for (int i=0; i<m; i++)
+            {
+                matrix[0][i]=0;
+            }
         }
-        if(r){
-            for(int j = 0; j < matrix[0].size(); j++) matrix[0][j] = 0;
+        if (c)
+        {
+            for (int i=0; i<n; i++)
+            {
+                matrix[i][0]=0;
+            }
         }
     }
 };
