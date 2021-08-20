a5aa19b77350a34b3d54e80e7e9bb38f55b4aaf5
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2021-08-20 03:33:13 +0000

    Sync LeetCode submission Runtime - 204 ms (5.04%), Memory - 133.3 MB (11.18%)

---
+++ 0131-palindrome-partitioning/solution.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string palindrome(string s,int i,int j)
+    {
+        string ans="";
+        int end=j;
+        while (i<end)
+        {
+            if (s[i]!=s[end])
+            {
+                return "";
+            }
+            else
+            {
+                ans+=s[i];
+            }
+            i++;
+            end--;
+        }
+        for (int l=i; l<=j; l++)
+        {
+            ans+=s[l];
+        }
+        return ans;
+    }
+    void partitionUtil(string s,int i,int j,vector<vector<string>> &ans,vector<string> temp)
+    {
+        if (i>j)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        else if (i==j)
+        {
+            string temp_ans="";
+            temp_ans+=s[i];
+            temp.push_back(temp_ans);
+            ans.push_back(temp);
+            return;
+        }
+        for (int k=i; k<=j; k++)
+        {
+            if (palindrome(s,i,k)!="")
+            {
+                temp.push_back(palindrome(s,i,k));
+                partitionUtil(s,k+1,j,ans,temp);
+                temp.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        int n=s.size()-1;
+        partitionUtil(s,0,n,ans,temp);
+        return ans;
+    }
+};
