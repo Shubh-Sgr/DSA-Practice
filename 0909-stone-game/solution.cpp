a59d4ced5906a73134baa76f70b8ccc8b67ebef3
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2022-01-21 03:33:06 +0000

    Sync LeetCode submission Runtime - 15 ms (15.32%), Memory - 8.7 MB (100.00%)

--- 0909-stone-game/solution.cpp
+++ 0909-stone-game/solution.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+    int dp[501][501];
+    
+    int Util(int l, int r, vector<int>& piles)
+    {
+        if (l>r)
+        {
+            return 0;
+        }
+        if (dp[l][r]!=-1)
+        {
+            return dp[l][r];
+        }
+        int leftChoosen = piles[l] + max(Util(l+2,r,piles),Util(l+1,r-1,piles));
+        int rightChoosen = piles[r] + max(Util(l+1,r-1,piles),Util(l,r-2,piles));
+        
+        return dp[l][r]=max(leftChoosen,rightChoosen);
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int l=0,r=piles.size()-1;
+        memset(dp,-1,sizeof(dp));
+        int alex=Util(l,r,piles);
+        int total_sum=0;
+        for (int i=0; i<=r; i++)
+        {
+            total_sum+=piles[i];
+        }
+        return alex>total_sum-alex;
     }
 };
