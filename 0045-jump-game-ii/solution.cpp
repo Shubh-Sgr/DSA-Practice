24e0545091a089edec8ec943f398fcc82c643db1
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2022-05-17 03:56:39 +0000

    Sync LeetCode submission Runtime - 1448 ms (5.02%), Memory - 16.7 MB (100.00%)

---
+++ 0045-jump-game-ii/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int dp[n];
+        
+        memset(dp,10000,sizeof(dp));
+        
+        dp[0]=0;
+        
+        for (int i=1; i<n; i++)
+        {
+            for (int j=0; j<i; j++)
+            {
+                if (j+nums[j]>=i){
+                    dp[i]=min(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
