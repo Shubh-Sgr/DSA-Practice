afa4199d17c0ab04378f531ed8e5875b8295d34d
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2022-05-17 03:35:28 +0000

    Sync LeetCode submission Runtime - 114 ms (15.02%), Memory - 48.5 MB (99.99%)

--- 0055-jump-game/solution.cpp
+++ 0055-jump-game/solution.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-       int n=nums.size();
-       int max=nums[0];
+        int max=nums[0];
+        int n= nums.size();
+        if (n==1)
+        {
+            return true;
+        }
         
-       for (int i=0; i<=max; i++)
-       {
-           max=nums[i]+i>max?nums[i]+i:max;
-           if (max>=n-1)
-           {
-               return true;
-           }
-       } 
+        for (int i=0; i<=max; i++)
+        {
+            max=max<i+nums[i]?i+nums[i]:max;
+            if (max>=n-1)
+            {
+                return true;
+            }
+        }
+        
         return false;
     }
 };
