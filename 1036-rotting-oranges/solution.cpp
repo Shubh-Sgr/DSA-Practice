21050ff51f4f938c0650a223683c0a6575c5f7a4
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2022-11-03 03:55:58 +0000

    Sync LeetCode submission Runtime - 23 ms (5.10%), Memory - 13.1 MB (100.00%)

--- 1036-rotting-oranges/solution.cpp
+++ 1036-rotting-oranges/solution.cpp
@@ -1,47 +1,55 @@
 class Solution {
 public:
-    
-   void is_valid(vector<vector<int>>& grid,int x,int y,int &countFreshOranges,queue<pair<int,int>> &qu){
-    //if any of the condition is ture then we need to return back..
-    if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size() || grid[x][y]==2 || grid[x][y]==0 ) return;
-    
-    grid[x][y]=2;
-    countFreshOranges--;
-    qu.push({x,y});  
-}
-  
-int orangesRotting(vector<vector<int>>& grid) {
-
-if(grid.empty()) return 0; // if there is no orange.
-
-int n=grid.size();    
-int m=grid[0].size();
-queue<pair<int,int>> qu; // queue to store the index of the cell where rotten oranges are placed.
-
-int countFreshOranges=0,time=0;   
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j]==2) qu.push({i,j});
-            if(grid[i][j]==1) countFreshOranges++;    
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        queue<pair<int,int>> q;
+        int time = -1;
+        int fresh=0;
+        int xDir[] = {1,-1,0,0};
+        int yDir[] = {0,0,1,-1};
+        
+        for (int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                if (grid[i][j]==2){
+                    q.push({i,j});
+                }
+                else if (grid[i][j]==1){
+                    fresh++;
+                }
+            }
         }
+        
+        while (!q.empty()){
+            int qS = q.size();
+            
+            while (qS>0){
+            pair<int,int> p = q.front();
+            q.pop();
+            int x = p.first;
+            int y = p.second;
+            
+            int cX,cY;
+            for (int i=0; i<4; i++){
+                cX=x+xDir[i];
+                cY = y+yDir[i];
+                if (cX>=0 && cX<n && cY>=0 && cY<m && grid[cX][cY]==1){
+                    grid[cX][cY]=2;
+                    q.push({cX,cY});
+                    fresh--;
+                }
+            }
+                qS--;
+            }
+            time++;
+        }
+        if (fresh>0){
+            return -1;
+        }
+        if (time==-1)
+            return 0;
+        return time;
+        
     }
-if(countFreshOranges==0) return 0;    
- // four adjacent positions at which the oranged placed will get rotten.
- // vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
- while(!qu.empty()&& countFreshOranges != 0){
-    time++; 
-    int size=qu.size(); 
-    for(int i=0;i<size;i++){
-        int x = qu.front().first;
-        int y = qu.front().second;
-        qu.pop();
-        is_valid(grid,x,y+1,countFreshOranges,qu); 
-        is_valid(grid,x,y-1,countFreshOranges,qu);
-        is_valid(grid,x+1,y,countFreshOranges,qu); 
-        is_valid(grid,x-1,y,countFreshOranges,qu);   
-    }    
- }     
-    if(countFreshOranges == 0 ) return time;
-    else return -1;
-}
 };
