3a4b36c1846652fa3f7ce806644d82307c8b44f3
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2022-11-21 02:07:35 +0000

    Sync LeetCode submission Runtime - 90 ms (33.53%), Memory - 10.8 MB (88.40%)

--- 0516-longest-palindromic-subsequence/solution.cpp
+++ 0516-longest-palindromic-subsequence/solution.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    static int max(int a,int b){
-        return (a>b?a:b);
-    }
     int longestPalindromeSubseq(string s) {
-        string s1=s;
-        reverse(s.begin(),s.end());
-        int n=s.size();
+        int n = s.size();
+        string r = s;
+        reverse(r.begin(),r.end());
+        
         int dp[n+1][n+1];
+        
         for (int i=0; i<=n; i++){
-            dp[i][0]=0;
-            dp[0][i]=0;
+            dp[i][0] = 0;
+            dp[0][i] = 0;
         }
         
         for (int i=1; i<=n; i++){
-            for (int j=1; j<=n; j++){
-                if (s1[i-1]==s[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+            for(int j=1; j<=n; j++){
+                if (s[i-1]==r[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
