3573c3ee8fbd3d7e0b0c2adbcb31545888063254
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2023-02-24 03:26:33 +0000

    Sync LeetCode submission Runtime - 3 ms (0.42%), Memory - 9.7 MB (100.00%)

--- 0226-invert-binary-tree/solution.cpp
+++ 0226-invert-binary-tree/solution.cpp
@@ -12,15 +12,13 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (root==NULL)
-        {
+        if (!root){
             return root;
         }
-        invertTree(root->left);
-        invertTree(root->right);
-        TreeNode* temp = root->left;
-        root->left = root->right;
-        root->right = temp;
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
         return root;
     }
 };
