d73bff2eed330d674fa1bd44f2cdfbcd11801eac
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2023-06-06 02:25:55 +0000

    Sync LeetCode submission Runtime - 10 ms (0.15%), Memory - 9.5 MB (100.00%)

--- 0074-search-a-2d-matrix/solution.cpp
+++ 0074-search-a-2d-matrix/solution.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (target<matrix[0][0])
-        {
+        int m = matrix.size();
+        if (m==0){
             return false;
         }
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int row_to_srch=-1;
-        for (int i=0; i<n-1; i++)
-        {
-            if (target>=matrix[i][0] && target<matrix[i+1][0])
-            {
-                row_to_srch=i;
+        int n = matrix[0].size();
+        int xCurr = 0;
+        int yCurr = n-1;
+        bool flag= false;
+
+        while (xCurr <m && yCurr>=0){
+            if (matrix[xCurr][yCurr]>target){
+                yCurr--;
+            }
+            else if (matrix[xCurr][yCurr]<target){
+                xCurr++;
+            }
+            else if (matrix[xCurr][yCurr]==target){
+                xCurr++;
+                flag=true;
                 break;
             }
         }
-        if (row_to_srch==-1)
-        {
-            row_to_srch=n-1;
-        }
-        // cout<<row_to_srch<<" ";
-        int st=0;
-        int mid,end=m-1;
-        while (st<=end)
-        {
-            mid=(st+end)/2;
-            if (matrix[row_to_srch][mid]==target)
-            {
-                return true;
-            }
-            else if(matrix[row_to_srch][mid]>target)
-            {
-                end=mid-1;
-            }
-            else
-            {
-                st=mid+1;
-            }
-        }
-        return false;
+        return flag;
     }
 };
