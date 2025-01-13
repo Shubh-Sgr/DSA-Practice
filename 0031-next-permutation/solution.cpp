f19f1413cfc8f1f11de579deecb18a066b2f004d
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2025-01-13 03:30:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.8 MB (54.36%)

--- 0031-next-permutation/solution.cpp
+++ 0031-next-permutation/solution.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int piv=n;
+        int pivot = -1;
 
         for (int i=n-1; i>0; i--){
-            if (nums[i]>nums[i-1]){
-                piv=i-1;
+            if (nums[i] > nums[i-1]){
+                pivot = i-1;
                 break;
             }
         }
 
-        for (int i=n-1; i>piv; i--){
-            if (nums[piv]<nums[i]){
-                int temp=nums[piv];
-                nums[piv] = nums[i];
-                nums[i]=temp;
-                sort(nums.begin()+piv+1,nums.end());
+        if (pivot == -1){
+            sort(nums.begin(),nums.end());
+            return;
+        }
+
+        for (int i=n-1; i>=0; i--){
+            if (nums[pivot] < nums[i]){
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+                sort(nums.begin()+pivot+1,nums.end());
                 break;
             }
         }
-        if (piv==n){
-            sort(nums.begin(),nums.end());
-        }   
-    } 
+    }
 };
