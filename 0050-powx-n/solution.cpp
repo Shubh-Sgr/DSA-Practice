6e52c70824808dfca1196963e33fe2dd6ecf69d6
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2025-12-23 12:59:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (44.44%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -3,16 +3,17 @@ public:
     double myPow(double x, int n) {
         double ans = 1.00;
         long long m = n;
-        m = m<0?(-1)*m:m;
-        while (m != 0){
-            if (m%2==0){
-                x = x*x;
-                m=m/2;
-            }else{
-                ans=ans*x;
-                m--;
-            }
+        m = m<0?abs(m):m;
+        while (m!=0){
+           if (m%2 ==0){
+             x=x*x;
+             m=m/2;
+           }else{
+            ans = ans*x;
+            m--;
+           }
         }
-        return n<0 ? 1.00/ans : ans;
+
+        return n<0?1.00/ans:ans;
     }
 };
