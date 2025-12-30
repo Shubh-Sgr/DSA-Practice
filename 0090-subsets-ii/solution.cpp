d771c5b4884460053cc882362b53188d14c7980b
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2025-12-30 12:00:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.3 MB (16.54%)

--- 0090-subsets-ii/solution.cpp
+++ 0090-subsets-ii/solution.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void calcSubsets(vector<int> nums,vector<vector<int>> &s,int i,int n,vector<int> temp)
-    {
-        s.push_back(temp);
-        for (int j=i; j<n; j++)
-        {
-            if (j == i || nums[j] != nums[j - 1]) { 
-				temp.push_back(nums[j]);
-				calcSubsets(nums,s,j+1,n,temp);
-				temp.pop_back();
-			}
+    static void subSumUtilDup(vector<int> nums, int ind, vector<int> temp, vector<vector<int>> &ans, int n){
+            ans.push_back(temp);
+
+        for (int i=ind; i<n; i++){
+            if (i==ind || nums[i]!= nums[i-1]){
+                temp.push_back(nums[i]);
+                subSumUtilDup(nums,i+1,temp,ans,n);
+                temp.pop_back();
+            }
         }
     }
-        
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> v;
-        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
         vector<int> temp;
-        int n=nums.size();
-        calcSubsets(nums,v,0,n,temp);
-        return v;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        subSumUtilDup(nums,0,temp,ans,n);
+        return ans;
     }
 };
