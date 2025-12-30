b63dcac7c94132a8ac25daab2bfe1e026313e6d0
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2025-12-30 11:05:10 +0000

    Sync LeetCode submission Runtime - 25 ms (5.76%), Memory - 44.8 MB (47.94%)

--- 0455-assign-cookies/solution.cpp
+++ 0455-assign-cookies/solution.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int cookSize = s.size();
+        // int cookSize = s.size();
+        // int childLen = g.size();
+
+        // sort(g.begin(),g.end());
+        // sort(s.begin(),s.end());
+
+        // int num = 0;
+        // int i = cookSize-1;
+        // int j = childLen-1;
+
+        // while (i>=0 && j>=0){
+        //     if (s[i] >= g[j]){
+        //         num++;
+        //         i--;
+        //         j--;
+        //     }else{
+        //         j--;
+        //     }
+        // }
+        // return num;
+
         int childLen = g.size();
-
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-
+        int cookieSize = s.size();
         int num = 0;
-        int i = cookSize-1;
-        int j = childLen-1;
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = childLen-1;
+        int j = cookieSize - 1;
 
         while (i>=0 && j>=0){
-            if (s[i] >= g[j]){
+            if (s[j]>=g[i]){
                 num++;
                 i--;
                 j--;
             }else{
-                j--;
+                i--;
             }
         }
         return num;
