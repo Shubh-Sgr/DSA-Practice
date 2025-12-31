84aba05c85e49d14b64f383e9f200ac779cb1381
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2025-12-31 07:38:17 +0000

    Sync LeetCode submission Runtime - 31 ms (13.37%), Memory - 22.5 MB (13.82%)

--- 0039-combination-sum/solution.cpp
+++ 0039-combination-sum/solution.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    
-    void combinationSumUtil(vector<int> nums,vector<int> temp,vector<vector<int>> &s,int i,int n,int target)
-    {
-        if (target==0)
-        {
-            s.push_back(temp);
-            return ;
+    static void combinationSumutil(vector<int>& candidates, int ind, vector<int> temp, int target,vector<vector<int>> &ans, int n){
+        if (target == 0){
+            ans.push_back(temp);
+            return;
         }
-        else if (target>0)
-        {
-            for (int j=i; j<n; j++)
-            {
-                // if (j==i || nums[j]!=nums[j-1])
-                // {
-                    temp.push_back(nums[j]);
-                    combinationSumUtil(nums,temp,s,j,n,target-nums[j]);
-                    temp.pop_back();
-                // }
+        else if (target > 0){
+            for (int i=ind; i<n; i++){
+                temp.push_back(candidates[i]);
+                combinationSumutil(candidates, i, temp, target-candidates[i], ans, n);
+                temp.pop_back();
             }
         }
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        sort(candidates.begin(),candidates.end());
-        int n=candidates.size();
-        combinationSumUtil(candidates,temp,ans,0,n,target);
+        int n = candidates.size();
+        sort(candidates.begin(), candidates.end());
+        combinationSumutil(candidates, 0, temp, target, ans, n);
         return ans;
     }
 };
