b8acb7a1979e5d506663d27de1e16d1bbf4d44a6
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-01-01 12:28:47 +0000

    Sync LeetCode submission Runtime - 7 ms (16.33%), Memory - 13.3 MB (9.94%)

--- 0051-n-queens/solution.cpp
+++ 0051-n-queens/solution.cpp
@@ -1,63 +1,52 @@
 class Solution {
 public:
-    
-    bool isSafe(int i,int j,int col[],int n)
-    {
-        for (int k=0; k<i; k++)
-        {
-            if (j==col[k] || abs(k-i)==abs(j-col[k]))
-                return false;
+    static bool isSafe(int i, int j, int posArr[], int n){
+        bool safe = true;
+        for (int k = 0; k<i; k++){
+            if (abs(posArr[k]-j) == abs(k-i) || j==posArr[k]){
+                safe = false;
+                break;
+            }
         }
-        return true;
+        return safe;
     }
-    
-    void solveNQueensUtil(vector<int> temp,vector<vector<string>> &ans,int n,int col[],int i)
-    {
-        if (i==n)
-        {
-            string s;
-            vector<string> Ans;
-            for (int j=0; j<n; j++)
-            {
-                s="";
-                for (int k=0; k<n; k++)
-                {
-                    if (k==temp[j])
-                    {
-                        s+="Q";
-                    }
-                    else
-                    {
-                        s+=".";
+
+    static void solveUtil(int n, int row, int posArr[], vector<int> temp, vector<vector<string>> &ans){
+        if (row == n){
+            vector<string> newTemp;
+            for (int i=0; i<n; i++){
+                string rowStr;
+                for (int j=0; j<n; j++){
+                    if (temp[i]==j){
+                        rowStr+='Q';
+                    }else{
+                        rowStr+='.';
                     }
                 }
-                Ans.push_back(s);
+                newTemp.push_back(rowStr);
             }
-            ans.push_back(Ans);
+            ans.push_back(newTemp);
             return;
         }
-        for (int j=0; j<n; j++)
-        {
-            if (isSafe(i,j,col,n))
-            {
-                col[i]=j;
-                temp.push_back(j);
-                solveNQueensUtil(temp,ans,n,col,i+1);
+
+        for (int i=0; i<n; i++){
+            if (isSafe(row,i, posArr, n)){
+                posArr[row] = i;
+                temp.push_back(i);
+                solveUtil(n, row+1, posArr, temp, ans);
+                posArr[row] = -1;
                 temp.pop_back();
-                col[i]=-1;
             }
         }
     }
-    
     vector<vector<string>> solveNQueens(int n) {
-       vector<int> temp;
-       vector<vector<string>> ans;
-        int col[n];
-        for (int i=0; i<n; i++)
-        {
-            col[i]=-1;
+        vector<int> temp;
+        vector<vector<string>> ans;
+         int posArr[n];
+        for (int i=0; i<n; i++){
+            posArr[i] = -1;
         }
-        solveNQueensUtil(temp,ans,n,col,0);
+        solveUtil(n, 0, posArr, temp, ans);
         return ans;
     }
 };
