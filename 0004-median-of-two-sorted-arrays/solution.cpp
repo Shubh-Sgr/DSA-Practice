4fa6daf6ba3986d3190a0c788f431c1e0299dd8d
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-01-02 12:48:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 95.1 MB (79.67%)

--- 0004-median-of-two-sorted-arrays/solution.cpp
+++ 0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       if(nums1.size()>nums2.size()) return findMedianSortedArrays(nums2, nums1);
-        int n1=nums1.size();
-        int n2=nums2.size();
-        
-        int low=0;
-        int high=n1;
-        
-        while (low<=high)
-        {
-            int cut1=(low+high)/2;
-            
-            int l1= cut1==0 ? INT_MIN:nums1[cut1-1];
-            int r1= cut1==n1 ? INT_MAX:nums1[cut1];
-            
-            int cut2=(n1+n2+1)/2 - cut1;
-            
-            int l2= cut2==0 ? INT_MIN:nums2[cut2-1];
-            int r2= cut2==n2 ? INT_MAX:nums2[cut2];
-            
-            if (l1>r2)
-            {
-                high=cut1-1;
-            }
-            else if (l2>r1)
-            {
-                low=cut1+1;
-            }
-            else
-            {
-                if ((n1+n2)%2==1)
+        if (nums1.size() > nums2.size()){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        int low = 0;
+        int high = n1;
+
+        while (low<=high){
+            int cut1 = low + (high-low)/2;
+            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int r1 = cut1 == n1 ? INT_MAX: nums1[cut1];
+
+            int cut2 = (n1+n2+1)/2-cut1;
+            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            int r2 = cut2 == n2 ? INT_MAX: nums2[cut2];
+
+            if (l1 > r2){
+                high = cut1-1;
+            }else if (l2> r1){
+                low = cut1+1;
+            }else{
+                if ((n1+n2)%2 == 1){
                     return max(l1,l2);
-                else
-                    return (max(l1,l2)+min(r1,r2))/2.0;
+                }else{
+                    return (max(l1,l2)+min(r2,r1))/2.0;
+                }
             }
         }
         return 0.0;
-        
     }
 };
