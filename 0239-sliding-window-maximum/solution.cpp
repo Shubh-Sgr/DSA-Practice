8cdf3be3f1d2ade8088d395255ff5866d87d1b2b
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-01-08 10:41:09 +0000

    Sync LeetCode submission Runtime - 28 ms (38.81%), Memory - 139 MB (92.08%)

--- 0239-sliding-window-maximum/solution.cpp
+++ 0239-sliding-window-maximum/solution.cpp
@@ -3,17 +3,22 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> dq;
         vector<int> ans;
-        for(int i =0;i<k;i++){
-            while(!dq.empty()&& nums[i]>=nums[dq.back()])
+        int n = nums.size();
+
+        for (int i=0; i<k; i++){
+            while (!dq.empty() && nums[dq.back()]<= nums[i]){
                 dq.pop_back();
+            }
             dq.push_back(i);
         }
         ans.push_back(nums[dq.front()]);
-        for(int i=k;i<nums.size();i++){
-            if(i-dq.front()>=k)
+        for (int i=k; i<n; i++){
+            if (i-dq.front()>=k){
                 dq.pop_front();
-             while(!dq.empty()&& nums[i]>=nums[dq.back()])
+            }
+            while (!dq.empty() && nums[dq.back()]<= nums[i]){
                 dq.pop_back();
+            }
             dq.push_back(i);
             ans.push_back(nums[dq.front()]);
         }
