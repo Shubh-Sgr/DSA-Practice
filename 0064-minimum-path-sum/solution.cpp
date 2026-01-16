338e58697e02d935ce1fdc9df92f7e4c77838fee
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-01-16 12:40:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.4 MB (84.31%)

--- 0064-minimum-path-sum/solution.cpp
+++ 0064-minimum-path-sum/solution.cpp
@@ -3,26 +3,26 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int dp[m][n];
-        dp[0][0]=grid[0][0];
-        
-        for (int i=1; i<m; i++)
-        {
-            dp[i][0]=dp[i-1][0]+grid[i][0];
+
+        int dp[201][201];
+
+        int sum = 0;
+        for (int i=0; i<n; i++){
+            sum+=grid[0][i];
+            dp[0][i] = sum;
         }
-        
-        for (int i=1; i<n; i++)
-        {
-            dp[0][i]=dp[0][i-1]+grid[0][i];
+        sum=0;
+        for (int i=0; i<m; i++){
+            sum+=grid[i][0];
+            dp[i][0] = sum;
         }
-        
-        for (int i=1; i<m; i++)
-        {
-            for (int j=1; j<n; j++)
-            {
-                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+
+        for (int i=1; i<m; i++){
+            for (int j=1; j<n; j++){
+                dp[i][j] = min(dp[i-1][j],dp[i][j-1])+grid[i][j];
             }
         }
+
         return dp[m-1][n-1];
     }
 };
