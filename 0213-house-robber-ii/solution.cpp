3a14345aa313fe68b9d6954be01fb963ecc31e48
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-02-10 04:42:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.2 MB (75.09%)

--- 0213-house-robber-ii/solution.cpp
+++ 0213-house-robber-ii/solution.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    
-    int Util(vector<int> nums, int l, int r)
-    {
-        int pre=0,curr=0,temp=0;
-        for (int i=l; i<=r; i++)
-        {
-            temp=max(pre+nums[i],curr);
-            pre=curr;
-            curr=temp;
-        }
-        return curr;
-    }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if (n==1)
-        {
-            return nums[0];
-        }
-        return max(Util(nums,0,n-2),Util(nums,1,n-1));    
+    int n = nums.size();
+
+       if (n==1){
+         return nums[0];
+       } 
+       else if (n==2){
+            return max(nums[0],nums[1]);
+       }
+       int prev1 = nums[0];
+       int curr1 = max(nums[0],nums[1]);
+       int temp;
+       for (int i=2; i<n-1; i++){
+        temp = curr1;
+        curr1 = max(curr1, prev1+nums[i]);
+        prev1 = temp;
+       }
+
+       int prev2 = nums[1];
+       int curr2 = max(nums[1],nums[2]);
+       for (int i=3; i<n; i++){
+        temp = curr2;
+        curr2 = max(curr2, prev2+nums[i]);
+        prev2 = temp;
+       }
+       return max(curr1,curr2);
     }
 };
