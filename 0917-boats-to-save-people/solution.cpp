53e53e1821f88cd4829066512f66b07b7e240ce9
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-06-09 05:33:04 +0000

    Sync LeetCode submission Runtime - 19 ms (48.56%), Memory - 45.9 MB (38.12%)

---
+++ 0917-boats-to-save-people/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int boats = 0;
+        int n = people.size();
+        sort(people.begin(),people.end());
+        int l = 0,r=n-1;
+        while(l<=r){
+            if (people[l]+people[r]<=limit){
+                boats++;
+                l++;
+                r--;
+            }else if(people[r]<=limit){
+                r--;
+                boats++;
+            }
+        }
+        return boats;
+    }
+};
