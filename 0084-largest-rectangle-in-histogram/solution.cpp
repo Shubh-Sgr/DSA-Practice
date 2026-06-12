4f757f6d4989833b0748f822afd2e6cc710078d1
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-06-12 11:10:06 +0000

    Sync LeetCode submission Runtime - 15 ms (86.01%), Memory - 81.2 MB (84.36%)

--- 0084-largest-rectangle-in-histogram/solution.cpp
+++ 0084-largest-rectangle-in-histogram/solution.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-         int n = heights.size();
-        stack < int > st;
-        vector<int> leftMin(n), rightMin(n);
-        if (heights.size()==1){
-            return heights[0];
-        }
+        stack<int> st;
+        heights.push_back(0);   // Sentinel
+        int maxArea = 0;
 
-        for (int i=0; i<n; i++){
-            while (!st.empty() && heights[st.top()]>=heights[i]){
+        for (int i = 0; i < heights.size(); i++) {
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+                int h = heights[st.top()];
                 st.pop();
-            }
-            if (st.empty()){
-                leftMin[i] = -1;
-            }else{
-                leftMin[i] = st.top();
+                int width = st.empty() ? i : i - st.top() - 1;
+                maxArea = max(maxArea, h * width);
             }
             st.push(i);
         }
-
-        while (!st.empty()){
-            st.pop();
-        }
-
-        for (int i=n-1; i>=0; i--){
-            while (!st.empty() && heights[st.top()]>=heights[i]){
-                st.pop();
-            }
-            if (st.empty()){
-                rightMin[i] = n;
-            }else{
-                rightMin[i] = st.top();
-            }
-            st.push(i);
-        }
-
-        int maxArea = INT_MIN;
-        for (int i=0; i<n; i++){
-            cout<<"leftMin"<<leftMin[i]<<" "<<rightMin[i]<<" ";
-            maxArea = max(maxArea , heights[i]*((rightMin[i]-leftMin[i])-1));
-        }
+        heights.pop_back();
         return maxArea;
     }
 };
