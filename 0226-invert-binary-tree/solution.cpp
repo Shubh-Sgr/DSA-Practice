76c6d760485be2c6999336c94b6083c8f3a70828
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-08-02 04:43:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (88.04%)

--- 0226-invert-binary-tree/solution.cpp
+++ 0226-invert-binary-tree/solution.cpp
@@ -17,8 +17,8 @@ public:
         }
         TreeNode* left = invertTree(root->left);
         TreeNode* right = invertTree(root->right);
-        root->left = right;
         root->right = left;
+        root->left = right;
         return root;
     }
 };
