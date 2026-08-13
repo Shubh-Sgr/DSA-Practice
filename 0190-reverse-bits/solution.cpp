754c3f02665c8517a5d230ff09f6653b9c8db3be
Shubh-Sgr <40662583+Shubh-Sgr@users.noreply.github.com>
2026-08-13 06:35:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (73.26%)

--- 0190-reverse-bits/solution.cpp
+++ 0190-reverse-bits/solution.cpp
@@ -1,31 +1,11 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
-        int f=0;
-        int pos=0;
-        unsigned int ans=0;
-        for (int i=31; i>=0; i--)
-        {
-            if (!f)
-            {
-                if ((1<<i) & (n))
-                {
-                    f=1;
-                    // cout<<i<<" ";
-                    
-                }
-            }
-            if (f)
-            {
-                if ((1<<i) & (n)){
-                unsigned int sm=1;
-                sm=sm<<(pos);
-                ans=ans|sm;
-                }
-            }
-             pos++;
+    int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans = (ans << 1) | (n & 1); 
+            n >>= 1;
         }
-        // cout<<pos<<" ";
         return ans;
     }
 };
